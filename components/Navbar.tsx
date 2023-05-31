320bf6cbea9b23ebbad172abc43bce6fc459f6d2
Святослав Дикий <svatoslav_dykyi@MacBook-Pro-Svatoslav.local>
2023-05-31 08:58:22 +0300

    fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -77,7 +77,7 @@ const Navbar = () => {
             className="flex flex-row items-center gap-2 cursor-pointer relative"
           >
             <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
-              <img src="/images/default-blue.png" />
+              <img src="/images/default-blue.png" alt="Alt" />
             </div>
             <BsChevronDown
               className={`text-white transition ${
